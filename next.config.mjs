b8cd4548692b12e748b0ae43702583bc565e7dd8
juhlie <juhliezzz@gmail.com>
2026-04-25 08:39:04 +0200

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,27 @@
-import nextra from 'nextra';
-import fs from 'fs';
-import path from 'path';
+import nextra from 'nextra'
+import {readFileSync} from 'fs'
+import {join} from 'path'
+import {bundledLanguages} from 'shiki'
 
-const daisyGrammar = JSON.parse(
-  fs.readFileSync(path.resolve(process.cwd(), 'public/syntax/daisy.tmLanguage.json'), 'utf-8')
-);
+const daisy_grammar = JSON.parse(
+    readFileSync(join(process.cwd(), 'public/syntax/daisy.tmLanguage.json'), 'utf-8')
+)
+
+bundledLanguages['daisy'] = () => ({default: daisy_grammar})
 
 const withNextra = nextra({
-  mdxOptions: {
-    rehypePrettyCodeOptions: {
-      theme: {
-        light: 'catppuccin-latte',
-        dark: 'catppuccin-mocha'
-      },
-      langs: [
-        {
-          ...daisyGrammar,
-          name: 'daisy',
-          scopeName: 'source.daisy'
-        }
-      ]
+    mdxOptions: {
+        rehypePrettyCodeOptions: {
+            theme: {
+                light: 'catppuccin-latte',
+                dark: 'catppuccin-mocha',
+            },
+        },
     },
-  },
 })
 
 export default withNextra({
-  output: 'export',
-  basePath: '/daisy',
-  images: {
-    unoptimized: true,
-  }
+    output: 'export',
+    basePath: '/daisy',
+    images: {unoptimized: true},
 })
